5c6f354d34d2c54d5a0713417479cc677427d945
fxt <1135309431@qq.com>
2018-03-27 09:18:19 -0700

    [max]submit answer 1~4.py

---
+++ task/max/python_test/3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+z=0
+while (1):
+	z=z+1
+        j=0
+	while(1):
+		j=j+1
+		if(j>=z):break
+                elif z*z-j*j==168:
+                	print "x=",z*z-268
+			break
+	if z*z-j*j==168:
+		break
+
