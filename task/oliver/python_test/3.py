72eb660228521c1d26df0ffa021cc15cba05fd00
oliver.he <a578997662@qq.com>
2018-03-28 08:41:22 -0700

    [oliver.he]refie 3.py and submit 5.py

--- task/oliver/python_test/3.py
+++ task/oliver/python_test/3.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
-
-num_str = raw_input("pelase a num\n")
-num = int(num_str)
-
-output = num ** 0.5
-
-if output - int(output) <= 0:
-	print output
-else:
-	print "This number can't be squared."
-
+num = 0
+while(1):
+	num += 1	
+	num_1 = (num + 100) ** 0.5
+	num_2 = (num + 268) ** 0.5
+	if num_1 - int(num_1) <= 0 and num_2 - int(num_2) <= 0:
+                print str(num) + '\n'
+                if num > 100000:
+                	break
